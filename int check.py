24b77c04dfc5dbdc0596ee2b089c9ee3809e0b5e
150610 <henryp4211@masseyhigh.school.nz>
2025-09-19 14:28:41 +1200

    Add files via upload

---
+++ int check.py
@@ -0,0 +1,33 @@
+# Ask user for width and loop until they
+# enter a number that is more than zero
+def int_check(question,low):
+    error = "Please enter a number that is more than or equal to {low}\n"
+    while True:
+
+        try:
+            # ask the user for a number
+            response = int(input(question))
+
+            # check that the number is more than zero
+            if response >= low:
+                return response
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+# Main Routine goes here
+for item in range(0, 2):
+    integer = int_check("Integer:",0)
+    print(integer)
+
+print()
+
+for item in range(0, 2):
+        height = int_check("Width:", 0)
+        print(height)
+
+for item in range(0, 2):
+        height = int_check("Height:", 0)
+        print(height)
